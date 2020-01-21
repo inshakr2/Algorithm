0bddc6e0fa6253d9678b7c607664ea8a8f700a9f
Chany <inshakr2@naver.com>
2020-01-21 16:42:24 +0900

    Update step_수학2.py

--- BOJ/step_수학2.py
+++ BOJ/step_수학2.py
@@ -117,3 +117,32 @@ while True:
         print(len(prime))
 
 
+
+# 골드바흐의 추측
+# 최대값인 10000까지의 소수 리스트를 우선 만들어 놓는다.
+# 그 차이가 최소인 소수들만 출력해야하기 때문에 10의 경우 5+5가 최소
+# 이처럼 가운데서부터 다음 소수 그다음 소수를 찾아가는 것이 문제가 원하는
+# 답을 찾는 방법
+# 입력값 n의 중간에서 부터 시작해서 소수 리스트의 값에 값이 있으면 그것은 소수이기
+# 때문에 중간 값에서부터 차근차근 하나씩 빼가며 소수를 찾고, 그 소수를
+# 입력값 n에서 뺀 값이 소수인지 판단하고 그게 참이면 출력하는 형태
+# 그리고 break를 하지 않으면 최소 값이아닌 모든 조합이 출력된다.
+
+N = 10000
+arr = [i for i in range(0,N+1)]
+arr[1] = 0
+for i in range(2,N+1):
+    if arr[i] == 0:
+        continue
+    for j in range(i*2,N+1,i):
+        arr[j] = 0
+
+case = int(input())
+for i in range(case):
+    n = int(input())
+    for i in range(n//2,1,-1):
+        if bool(arr[i]) :
+            if bool(arr[n-i]) :
+                print(i, n-i)
+                break
+    
