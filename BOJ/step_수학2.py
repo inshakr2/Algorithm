dddb02171442585d5d90030f088cf52552a73ebc
Chany <inshakr2@naver.com>
2020-01-24 10:10:30 +0900

    Update step_수학2.py

--- BOJ/step_수학2.py
+++ BOJ/step_수학2.py
@@ -190,5 +190,12 @@ while True:
         print('wrong')
 
 
+while True:
     
+    a, b, c = sorted(map(int,input().split(' ')))
+    
+    if sum([a,b,c]) == 0:
+        break
+    
+    print('right' if a**2 + b**2 == c**2 else 'wrong')
 
