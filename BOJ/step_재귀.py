f044955fa290182e638fa176618ba76dd6b2a0e9
Chany <inshakr2@naver.com>
2020-01-28 13:42:24 +0900

    Update step_재귀.py

--- BOJ/step_재귀.py
+++ BOJ/step_재귀.py
@@ -15,3 +15,15 @@ def factorial(n):
 
 n = int(input())
 print(factorial(n))
+
+
+# 피보나치 수
+def fibonacci(n):
+    if n == 0 :
+        return 0
+    elif n == 1 :
+        return 1
+    
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(int(input())))
