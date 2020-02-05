8ca5778df48c31ed67b8946eb4ff41d684daab31
Chany <inshakr2@naver.com>
2020-02-05 18:11:17 +0900

    Create step_정렬.py

---
+++ BOJ/step_정렬.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 18:02:16 2020
+
+@author: ChangYeol
+"""
+
+# 수 정렬하기
+N = int(input())
+
+arr = list()
+for i in range(N):
+    
+    arr.append(int(input()))
+
+arr.sort()
+
+for i in arr:
+    print(i)
