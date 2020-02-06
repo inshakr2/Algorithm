46155374b94ee3eee8ce5737c6943205f85e8f76
Chany <inshakr2@naver.com>
2020-02-06 11:14:36 +0900

    Update step_정렬.py

--- BOJ/step_정렬.py
+++ BOJ/step_정렬.py
@@ -17,3 +17,39 @@ arr.sort()
 
 for i in arr:
     print(i)
+    
+# 수 정렬하기 2
+N = int(input())
+
+arr = list()
+for i in range(N):
+    arr.append(int(input()))
+    
+# short coding
+arr = [int(input()) for i in range(int(input()))]
+print('\n'.join(map(str,sorted(arr))))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+for i in sorted(arr):
+    print(i)
