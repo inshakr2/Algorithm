cb1b267f2695193258a1d12f7cd6fc2b10257344
Chany <inshakr2@naver.com>
2020-02-09 15:14:39 +0900

    Update step_정렬.py

--- BOJ/step_정렬.py
+++ BOJ/step_정렬.py
@@ -97,3 +97,13 @@ print(mean(numbers))
 print(median(numbers))
 print(mode(numbers))
 print(scope(numbers))
+
+
+# 소트인사이드
+nums = input()
+nums = [int(n)  for n in nums]
+
+ordered_nums = sorted(nums, reverse=True)
+
+for n in ordered_nums : 
+    print(n, end="")
