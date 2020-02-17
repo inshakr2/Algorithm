a7dc2d01f02ffe5defa969c171d7cf969018694e
Chany <inshakr2@naver.com>
2020-02-17 21:53:05 +0900

    Update step_백트래킹.py

--- BOJ/step_백트래킹.py
+++ BOJ/step_백트래킹.py
@@ -108,3 +108,26 @@ used = [0] * N
 p = [0] * M
 npr(0, M, N)
 
+# N과 M 2
+# https://wlstyql.tistory.com/58?category=852442
+N, M = map(int, input().split())
+visited = [False] * N
+out, out_all = [], []
+
+def solve(depth, N, M):
+    if depth == M:
+        out_str = ' '.join(map(str, sorted(out)))
+        if out_str not in out_all:
+            out_all.append(out_str)
+        return
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            out.append(i+1)
+            solve(depth+1, N, M)
+            visited[i] = False
+            out.pop()
+solve(0, N, M)
+
+for i in out_all:
+    print(i)
