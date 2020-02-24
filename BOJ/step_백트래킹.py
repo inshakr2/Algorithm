34dc5fdf971e7e19d9c8ee41733d7ff553c5da80
Chany <inshakr2@naver.com>
2020-02-24 18:44:34 +0900

    Update step_백트래킹.py

--- BOJ/step_백트래킹.py
+++ BOJ/step_백트래킹.py
@@ -338,3 +338,30 @@ for case in operation_set:
     
 print(max_answer)
 print(min_answer)
+
+
+# 스타트와 링크
+
+import itertools
+import sys
+n=int(sys.stdin.readline().strip())
+temp=list(range(n))
+synergy=[]
+for i in range(n):
+    synergy.append(list(map(int,sys.stdin.readline().strip().split())))
+p = list(itertools.combinations(temp,n//2))
+answer=[]
+for team in p:
+    start = list(team)
+    link = list(set(temp)-set(team))
+    Synergy_start=0
+    Synergy_link=0
+    for i in start:
+        for j in start:
+            Synergy_start+=synergy[i][j]
+    for i in link:
+        for j in link:
+            Synergy_link+=synergy[i][j]
+    ans=abs(Synergy_link-Synergy_start)
+    answer.append(ans)
+print(min(answer))
