47538677bf82cad253c63b5660046560d95df99b
Chany <inshakr2@naver.com>
2020-02-27 23:31:39 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -32,3 +32,24 @@ zero_count, one_count = count_fibonacci(40)
 for _ in range(n):
     m = int(input())
     print("%d %d" % (zero_count[m], one_count[m]))
+    
+    
+# 01타일
+
+def tile001(n):
+    answer = 0
+    temp_1 = 1
+    temp_2 = 2
+    for i in range(1, n+1):
+        if i == 1:
+            answer = temp_1
+        elif i == 2:
+            answer = temp_2
+        else:
+            answer = temp_1 + temp_2
+            temp_1 = temp_2 % 15746
+            temp_2 = answer % 15746
+    
+    print(answer % 15746)
+        
+tile001(int(input()))
