29eba67937e863feed50b3e469def47d8d00c264
Chany <inshakr2@naver.com>
2020-02-29 17:40:58 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -56,8 +56,6 @@ def tile001(n):
 tile001(int(input()))
 
 
-
-
 # 파도반 수열
 
 t = int(input())
@@ -75,3 +73,22 @@ for _ in range(t):
     n = int(input())
     wave()
     print(dp[n])
+    
+    
+    
+# RGB거리
+def solve():
+    for i in range(3):
+        d[1][i] = p[1][i]
+    for i in range(2, n+1):
+        d[i][0] = min(d[i-1][1], d[i-1][2]) + p[i][0]
+        d[i][1] = min(d[i-1][0], d[i-1][2]) + p[i][1]
+        d[i][2] = min(d[i-1][0], d[i-1][1]) + p[i][2]
+    print(min(d[n]))
+
+n = int(input())
+d = [[0]*3 for _ in range(n+1)]
+p = [[0]*3]+[list(map(int, input().split())) for _ in range(n)]
+solve()
+
+
