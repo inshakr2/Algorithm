39ca4f74e73903b666a127c2a59eea5ed3bb2649
Chany <inshakr2@naver.com>
2020-03-04 23:28:57 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -154,3 +154,36 @@ for i in range(4, stair_count + 1):
                             stair_scores_list[i] + stair_scores_list[i - 1] + sum_of_score[i - 3]))
 
 print(sum_of_score[-1])
+
+
+
+# 1로 만들기
+def min(x, y):
+    return x if x <= y else y
+ 
+x = int(input())
+ 
+minimum_count = [ 0 for _ in range(x+1)]
+ 
+index = 0
+while(True):
+    if index > x:
+        break
+ 
+    if index <= 1:
+        minimum_count[index] = 0
+    else:
+        temp_min = x + 1
+        if index % 3 == 0:
+            temp_index = int(index/3)
+            temp_min = min(temp_min, minimum_count[temp_index])
+ 
+        if index % 2 == 0:
+            temp_index = int(index/2)
+            temp_min = min(temp_min, minimum_count[temp_index])
+ 
+        temp_min = min(temp_min, minimum_count[index-1])
+        minimum_count[index] = int(temp_min + 1)
+    index = index + 1
+ 
+print(minimum_count[x])
