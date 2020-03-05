5f8b1a121cdfee57d77474586c6d7dd102403538
Chany <inshakr2@naver.com>
2020-03-05 19:27:51 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -187,3 +187,22 @@ while(True):
     index = index + 1
  
 print(minimum_count[x])
+
+
+# 쉬운 계단 수
+stair_numbers = [[0 for _ in range(10)] for _ in range(101)]
+
+for i in range(1, 101):
+    for j in range(10):
+        if i == 1:
+            stair_numbers[i][j] = 1
+        else:
+            if 1 <= j <= 8:
+                stair_numbers[i][j] = stair_numbers[i-1][j-1] + stair_numbers[i-1][j+1]
+            elif j == 0:
+                stair_numbers[i][j] = stair_numbers[i-1][j+1]
+            elif j == 9:
+                stair_numbers[i][j] = stair_numbers[i-1][j-11]
+                
+N = int(input())             
+print(sum(stair_numbers[N][1:10]) % 1000000000)
