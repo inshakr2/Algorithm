411e056549a9a9e1d84ebf45548abe0cb2956722
Chany <inshakr2@naver.com>
2020-03-06 14:38:43 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -206,3 +206,22 @@ for i in range(1, 101):
                 
 N = int(input())             
 print(sum(stair_numbers[N][1:10]) % 1000000000)
+
+
+# 포도주 시식
+wine = int(input())
+wine_capa = [0]
+result = [0 for _ in range(wine + 1)]
+
+for _ in range(wine):
+    wine_capa.append(int(input()))
+
+for i in range(1, wine + 1):
+    if i == 1:
+        result[1] = wine_capa[1]
+    elif i == 2:
+        result[2] = wine_capa[1] + wine_capa[2]
+    else:
+        result[i] = max(result[i-3] + wine_capa[i-1] + wine_capa[i], result[i-2] + wine_capa[i], result[i-1])  
+
+print(result[i])
