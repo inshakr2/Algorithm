42b7983301629cefb41c51df200f5a30412c034b
Chany <inshakr2@naver.com>
2020-03-07 12:51:56 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -225,3 +225,26 @@ for i in range(1, wine + 1):
         result[i] = max(result[i-3] + wine_capa[i-1] + wine_capa[i], result[i-2] + wine_capa[i], result[i-1])  
 
 print(result[i])
+
+
+# 가장 긴 증가하는 부분 수열
+
+A = int(input())
+A_list = list(map(int, input().split()))
+result = [[] for _ in range(A)]
+
+for i in range(A):
+    if i == 0:
+        result[i].append(A_list[i])
+    else:
+        for j in range(0, i):
+            if result[j][-1] < A_list[i]: #이전 수열의 마지막 숫자가 나보다 작은가?
+                if len(result[i]) - 1 < len(result[j]): #수열의 길이가 현재값보다 작은가?
+                    result[i] = result[j] + [A_list[i]]
+        if not result[i]:
+            result[i].append(A_list[i]) #자신이 지금까지 최소값일 경우
+
+answer = 0
+for i in range(A):
+    answer = max(answer, len(result[i]))
+print(answer)
