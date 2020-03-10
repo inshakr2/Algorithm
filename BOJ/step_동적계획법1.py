90deb6d71f83ef51c18f518eaa89f3f0064165a1
Chany <inshakr2@naver.com>
2020-03-10 18:56:30 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -306,3 +306,21 @@ for i in range(A):
     
 print(answer - 1) #가운데 숫자는 두 번 더해지므로 -1
 
+
+
+# 전깃줄
+n = int(input())
+w = []
+w_b = []
+dp = [0 for i in range(n)]
+for i in range(n):
+    w.append(list(map(int, input().split())))
+w.sort(key = lambda x:x[0])
+for i in range(n):
+    w_b.append(w[i][1])
+for i in range(n):
+    for j in range(i):
+        if w_b[i] > w_b[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(n - max(dp))
