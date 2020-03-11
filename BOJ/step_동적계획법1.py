f226f54b940bd3ce1349f0ad5254b0ed42f58ca1
Chany <inshakr2@naver.com>
2020-03-11 22:54:18 +0900

    Update step_동적계획법1.py

--- BOJ/step_동적계획법1.py
+++ BOJ/step_동적계획법1.py
@@ -324,3 +324,24 @@ for i in range(n):
             dp[i] = dp[j]
     dp[i] += 1
 print(n - max(dp))
+
+
+# LCS
+A = list(str(input()))
+B = list(str(input()))
+            
+#배열 생성
+arr = [[0 for _ in range(len(A) + 1)] for _ in range(len(B) + 1)]
+answer = 0
+
+
+#규칙에 따라 순차적으로 숫자를 넣어준다.
+for i in range(len(A)):
+    for j in range(len(B)):
+        if A[i] == B[j]:
+            arr[j+1][i+1] = arr[j][i] + 1
+            answer = max(answer, arr[j+1][i+1])
+        else:
+            arr[j+1][i+1] = max(arr[j][i+1], arr[j+1][i])
+
+print(answer)
