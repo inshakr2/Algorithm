9dd173180775db84fd84a806193b08f419467a8e
Chany <inshakr2@naver.com>
2020-03-13 21:59:31 +0900

    Create step_실습1.py

---
+++ BOJ/step_실습1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+# 평균 점수 
+score_list = []
+for i in range(5):
+    score = int(input())
+    if score >= 40:
+        score_list.append(score)
+    else:
+        score_list.append(40)
+print(round(sum(score_list)/5))
+
