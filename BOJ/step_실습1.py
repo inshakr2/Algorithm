6023faaf229272e1f135efdc1e0f1d40ca46286c
Chany <inshakr2@naver.com>
2020-03-17 12:48:40 +0900

    Update step_실습1.py

--- BOJ/step_실습1.py
+++ BOJ/step_실습1.py
@@ -34,3 +34,13 @@ for i in range(1, N+1):
     
 for i in range(N-1, 0 , -1):
     print('*' * i)
+    
+    
+# 별찍기 9
+N = int(input())
+
+for i in range(N-1, 0, -1):
+    print(' '*(N-i-1) + ('*'*(2*i+1)))
+
+for i in range(N):
+    print(' '*(N-i-1) + ('*'*(2*i+1)))
