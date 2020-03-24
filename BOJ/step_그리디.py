6a8e89228c57b07ff2af1099fa68f792e806972c
Chany <inshakr2@naver.com>
2020-03-25 00:35:29 +0900

    Update step_그리디.py

--- BOJ/step_그리디.py
+++ BOJ/step_그리디.py
@@ -19,3 +19,21 @@ for i in range(len(value_list)):
     
 print(cnt)
 
+# ATM
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+if N == 1 : #1
+    print(nums[0])
+else : 
+    nums.sort() #2
+
+    i_sum = 0 
+    min_sum = 0
+
+    for i in range(N): 
+        min_sum += (i_sum + nums[i]) #3
+        i_sum += nums[i] #4
+        
+    print(min_sum)
