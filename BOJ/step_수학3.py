570aa3dc86536d7ad6e520d50b9b96db0f96d1d3
Chany <inshakr2@naver.com>
2020-03-30 22:07:54 +0900

    Update step_수학3.py

--- BOJ/step_수학3.py
+++ BOJ/step_수학3.py
@@ -17,3 +17,21 @@ while x != 0 and y != 0:
     
     x, y = map(int,input().split(' '))
     
+# 약수
+
+N = int(input())
+gcd_list = list(map(int, input().split()))
+gcd_list = sorted(gcd_list)
+answer = 0
+
+for i in range(len(gcd_list)):
+    answer += gcd_list[i] * gcd_list[-i-1]
+
+print(answer//len(gcd_list))
+
+
+n = int(input())
+a = list(map(int, input().split()))
+a_max = max(a)
+a_min = min(a)
+print(a_max * a_min)
