6b4146b6a2059732bd017d4b0bef62add7196766
Chany <inshakr2@naver.com>
2020-04-05 23:50:11 +0900

    Update

---
+++ BOJ/Java/입출력과 사칙연산/Java_1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Java_1000 {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+
+        int a = data.nextInt();
+        int b = data.nextInt();
+
+        System.out.println(a + b);
+    }
+}
