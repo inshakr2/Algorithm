7101c65876fb0d38bc8f149fe7bcd6f8df5ad4c2
Chany <inshakr2@naver.com>
2020-04-10 20:51:18 +0900

    update

---
+++ BOJ/Java/입출력과 사칙연산/Java_2588.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Java_2588 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        String b = input.next();
+
+        for (int i = b.length(); i < 1; i--){
+            a * b[i]
+
+        }
+    }
+}
