3cf21b76bae56cce28f27094241352222ba19ba9
Chany <inshakr2@naver.com>
2020-04-16 23:24:46 +0900

    Update

---
+++ BOJ/Java/IF/Java_2753.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Java_2753 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+
+        if( (a % 4 == 0) && (a % 100 != 0 | a % 400 == 0) ){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
