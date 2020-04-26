0f49bbf26f81845ba8d291562ec0fa2998e03e07
Chany <inshakr2@naver.com>
2020-04-26 22:53:32 +0900

    Update

---
+++ BOJ/Java/FOR/Java_2741.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Java_2741 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+
+        for(int i = 1; i <= N; i++){
+            System.out.println(i);
+        }
+    }
+}
