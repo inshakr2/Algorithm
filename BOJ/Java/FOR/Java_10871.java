0ed21f3b1c494834c42407881f62f286c8478c25
Chany <inshakr2@naver.com>
2020-05-08 19:53:57 +0900

    Update

--- BOJ/Java/FOR/Java_10871.java
+++ BOJ/Java/FOR/Java_10871.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Java_10871 {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
