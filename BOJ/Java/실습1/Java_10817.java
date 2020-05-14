3b01bb2761115acab73f5fcae6e8262b3b06331f
Chany <inshakr2@naver.com>
2020-05-15 00:22:44 +0900

    Update

---
+++ BOJ/Java/실습1/Java_10817.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Java_10817 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] pre_lst = br.readLine().split(" ");
+        int[] lst = new int[3];
+
+        for(int i = 0; i < 3; i++){
+            lst[i] = Integer.parseInt(pre_lst[i]);
+        }
+
+        Arrays.sort(lst);
+        System.out.println(lst[1]);
+
+    }
+}
