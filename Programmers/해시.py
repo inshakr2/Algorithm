cc072e30cd1e4eb5340074149de959daa7de62b2
Chany <inshakr2@naver.com>
2021-11-29 22:28:43 +0900

    Update 해시.py

--- Programmers/해시.py
+++ Programmers/해시.py
@@ -9,33 +9,34 @@ Created on Sat Jan 11 13:35:04 2020
 
 def solution(genres, plays):
     
-    import operator
-    gp_list = list()
+    # 장르 순으로 정렬
+    # 각 장르별 재생 횟수로 정렬
+    # -> 각 인덱스 공통으로 사용
+    
+    hash_dic = dict()
     answer = list()
-    
-    for i in range(len(genres)):
-        c_genre = genres[i]
-        c_plays = plays[i]
-        if gp_list.count(c_genre) == 0:
-            gp_list.append(c_genre)
-            gp_list.append([c_plays])
+    for idx, genre in enumerate(genres) :
+        if genre not in hash_dic.keys() :
+            hash_dic[genre] = {'total' : plays[idx], idx : plays[idx]}
+
         else :
-            gp_list[gp_list.index(c_genre)+1].append(c_plays)
-            
-    answer_dic = dict()
-    for i in range(1,len(gp_list),2):
-        val = (sorted(gp_list[i], reverse = True))
-        k = sum(val)
-    
-        answer_dic[k] = val
-    
-    answer_dic = sorted(answer_dic.items(), key=operator.itemgetter(1), reverse=True)
-    answer_play = list()
-    for i in range(len(answer_dic)):
-        for j in answer_dic[i][1][:2]:
-            answer_play.append(j)
-    
-    for i in answer_play:
-        answer.append(plays.index(i))
-    
+            hash_dic[genre]['total'] += plays[idx]
+            hash_dic[genre][idx] = plays[idx]        
+
+
+    sorted_hash_dic = sorted(hash_dic.items(), key = lambda x : x[1]['total'], reverse = True)
+
+    for i in range(len(sorted_hash_dic)):
+
+        tmp_dic = sorted_hash_dic[i][1]
+        sorted_play_list = sorted(tmp_dic.items(), key = lambda x : x[1], reverse = True)
+
+        dur = 1
+        while dur <= 2:
+            answer.append(sorted_play_list[dur][0])
+
+            if len(sorted_play_list) < 3:
+                break
+            dur += 1
+
     return answer
